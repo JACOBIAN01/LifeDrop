18a2be25ab1f76306282439e562380cb29d6a7f3
JACOBIAN01 <subhadeepghorai23@gmail.com>
2025-08-12 01:28:41 +0530

    Design Update

--- lifedrop-frontend/src/components/Navbar.jsx
+++ lifedrop-frontend/src/components/Navbar.jsx
@@ -10,8 +10,11 @@ const Navbar = () => {
     <nav className="bg-gradient-to-r from-rose-200 via-red-200 to-rose-300 shadow-sm">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
-          
           <div className="flex items-center">
+            <img
+              className="h-15 object-contain mr-3 rounded-lg"
+              src="/Logo_LifeDrop.png"
+            />
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-600 via-red-500 to-red-600 font-extrabold text-3xl tracking-wide">
               <Link to="/">LifeDrop</Link>
             </span>
@@ -19,13 +22,15 @@ const Navbar = () => {
 
           <div className="flex space-x-4">
             <button
-              onClick={()=>navigate("/auth")}
-             className="px-4 py-2 rounded-lg bg-white text-rose-500 font-semibold hover:bg-rose-50 transition">
+              onClick={() => navigate("/auth")}
+              className="px-4 py-2 rounded-lg bg-white text-rose-500 font-semibold hover:bg-rose-50 transition"
+            >
               Sign In
             </button>
             <button
-              onClick={()=>navigate("/auth")}
-             className="px-4 py-2 rounded-lg bg-rose-400 text-white font-semibold hover:bg-rose-500 transition">
+              onClick={() => navigate("/auth")}
+              className="px-4 py-2 rounded-lg bg-rose-400 text-white font-semibold hover:bg-rose-500 transition"
+            >
               Sign Up
             </button>
           </div>
