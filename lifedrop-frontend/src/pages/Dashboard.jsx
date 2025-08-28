e6cdc2f1fbbbdd6d3076b5d3a0bd3f51177575f0
JACOBIAN01 <subhadeepghorai23@gmail.com>
2025-08-28 16:29:10 +0530

    Enhance DonorDashboard with Matched Requests feature; add state management for matched requests and implement functionality to show interest in requests.

--- lifedrop-frontend/src/pages/Dashboard.jsx
+++ lifedrop-frontend/src/pages/Dashboard.jsx
@@ -17,7 +17,7 @@ export default function Dashboard() {
       <Navbar />
       <div className="max-w-4xl mx-auto px-4 py-10">
         <h1 className="text-2xl md:text-3xl font-bold text-red-800 text-center">
-          Welcome to the Dashboard {user.displayName || "User"}
+          Welcome to the Dashboard {user ? user.displayName||"User" : "User"}
         </h1>
 
         {isDonor ? (
