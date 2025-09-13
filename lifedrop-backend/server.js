2bc52640e4934fc54eb09bb0f48974dcfdb29679
JACOBIAN01 <subhadeepghorai23@gmail.com>
2025-09-13 19:59:34 +0530

    Update

--- lifedrop-backend/server.js
+++ lifedrop-backend/server.js
@@ -6,7 +6,6 @@ import twilio from "twilio";
 require("dotenv").config();
 
 // Twilio credentials
-
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
