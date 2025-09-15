ad36dad1a3a95df60562e166b04b59c38e7bd316
JACOBIAN01 <subhadeepghorai23@gmail.com>
2025-09-15 15:45:42 +0530

    Update

--- lifedrop-frontend/src/services/AuthService.js
+++ lifedrop-frontend/src/services/AuthService.js
@@ -7,8 +7,8 @@ import {
   onAuthStateChanged,
 } from "firebase/auth";
 import { setDoc, doc,getDoc } from "firebase/firestore";
-import { db } from "./firebase";
-import {auth } from "./firebase"; 
+import { db } from "./firebase.js";
+import {auth } from "./firebase.js"; 
 
 //Get Current User
 export const useCurrentUser = () => {
